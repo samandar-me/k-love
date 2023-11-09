fc72f738b43c6d031c8d26efed9caae8bc441f56
samandar-me <samandarcoderdev@gmail.com>
2023-11-09 21:57:10 +0500

    now

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -34,6 +34,8 @@ kotlin {
             implementation("io.ktor:ktor-client-serialization:$ktorVersion")
             implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
             implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+
+            implementation(libs.kotlinx.coroutines.core)
         }
     }
 }
